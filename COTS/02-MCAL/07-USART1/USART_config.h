a7657a1c3247ac3655e4bbe19096634bf44b9f12
menna687 <mennatahha@gmail.com>
2023-09-13 11:15:59 +0300

    Working MCAL and HAL(added LCD)

---
+++ COTS/02-MCAL/07-USART1/USART_config.h
@@ -0,0 +1,17 @@
+/*****************************************/
+/* Author  :  Gehad Elkoumy              */
+/* Version :  V01                        */
+/* Date    :  23 AUG 2023                */
+/*****************************************/
+
+#ifndef USART_CONFIG_H
+#define USART_CONFIG_H
+
+/* 
+	Options:		Baud_9600
+				Baud_112500 
+*/
+
+#define 	BaudRate	Baud_9600
+
+#endif
